0b14b2ed43621e24df4772c13532cca23bfcc56a
Maria <maria@rload.es>
2023-05-05 13:32:03 +0200

    slider impressions

--- components/ImpressionsSlider/ImpressionsSlider.tsx
+++ components/ImpressionsSlider/ImpressionsSlider.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 import style from "./impressions-slider.module.scss";
 import "swiper/css";
-import "swiper/css/effect-fade";
-import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade, Pagination } from "swiper";
+import { Pagination } from "swiper";
 import TextApp from "../TextApp";
 import Image from "next/image";
 import ButtonApp from "../ButtonApp";
 import arrow from "../../assets/img/arrow_icon.svg";
 import useComponentUtils from "@/ui/hooks/component.hooks";
-import parse from "html-react-parser";
 import image1 from "../../public/img/berlin/01.jpg";
 import image2 from "../../public/img/berlin/02.jpg";
 import image3 from "../../public/img/berlin/03.jpg";
@@ -21,13 +18,17 @@ interface ImpressionsSliderProps {
 }
 
 const ImpressionsSlider = ({ textData }: ImpressionsSliderProps) => {
-  //console.log(textData)
-  //checking mobile menu
   const { useWindowSize } = useComponentUtils();
-
-  const isMobile = useWindowSize().width <= 768 ? true : false;
+  const isMobile = useWindowSize().width <= 1080 ? true : false;
 
   const [swiper, setSwiper] = useState<any>();
+  const pagination = {
+    clickable: true,
+    bulletClass: "bullet",
+    renderBullet: (index: number, className: any) => {
+      return `<div aria-hidden=true class="${className} ${style.bullet}"></div>`;
+    }
+  };
 
   return (
     <div className={`${style.impressions} impressions`}>
@@ -36,38 +37,42 @@ const ImpressionsSlider = ({ textData }: ImpressionsSliderProps) => {
           <h2 className={style.title}>
             <TextApp labelID="impressions.title" textData={textData} />
           </h2>
-          <p><TextApp labelID="impressions.text" textData={textData}/></p>
+          <p>
+            <TextApp labelID="impressions.text" textData={textData} />
+          </p>
           <div className={style.button}>
-          <ButtonApp
-            labelID="learnMore.button.label"
-            onClick={() => console.log("hola")}
-            icon={arrow}
-            iconAlign="right"
-          />
-        </div>
+            <ButtonApp
+              labelID="learnMore.button.label"
+              onClick={() => console.log("hola")}
+              icon={arrow}
+              iconAlign="right"
+            />
+          </div>
         </article>
-
       </div>
       <div className={style.slider}>
-      <div className={style.slider_navigation}>
-        <button className={style.slider_navigation__next} onClick={() => swiper.slideNext()}>
-          <span className="only-readers">Next slide</span>
-        </button>
-        <button className={style.slider_navigation__prev} onClick={() => swiper.slidePrev()}>
-          <span className="only-readers">Previous slide</span>
-        </button>
-      </div>
-      <div className={style.slider_content}>
-        <Swiper
-          spaceBetween={20}
-          // centeredSlides={false}
-          // loopedSlides={8}
-          slidesPerView={"auto"}
-          pagination={{ clickable: true }}
-          onSwiper={(swiper) => setSwiper(swiper)}
-          onSlideChange={() => console.log("slide change")}
-        >
-          <div className={style.test}>
+        {!isMobile && (
+          <div className={style.slider_navigation}>
+            <button className={style.slider_navigation__next} onClick={() => swiper.slideNext()}>
+              <span className="only-readers">Next slide</span>
+            </button>
+            <button className={style.slider_navigation__prev} onClick={() => swiper.slidePrev()}>
+              <span className="only-readers">Previous slide</span>
+            </button>
+          </div>
+        )}
+
+        <div className={style.slider_content}>
+          <Swiper
+            // spaceBetween={5}
+            //centeredSlides={false}
+            // loopedSlides={8}
+            slidesPerView={"auto"}
+            pagination={pagination}
+            onSwiper={(swiper) => setSwiper(swiper)}
+            onSlideChange={() => console.log("slide change")}
+            modules={[Pagination]}
+          >
             <SwiperSlide>
               <Image src={image1} alt={"Definitio logo"} />
             </SwiperSlide>
@@ -80,10 +85,9 @@ const ImpressionsSlider = ({ textData }: ImpressionsSliderProps) => {
             <SwiperSlide>
               <Image src={image4} alt={"Definitio logo"} />
             </SwiperSlide>
-          </div>
-        </Swiper>
+          </Swiper>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
